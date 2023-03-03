5e6bdd209e8b881ccd5a657d95bac9f0695b3d48
Jaime Daza <jaimedavid250701@outlook.com>
2023-03-03 17:11:37 -0500

    refactor: Remove App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-
-  return (
-    <div className="App">
-    </div>
-  )
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
