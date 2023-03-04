6899e86971108508af25e06ca07ccdf2cf6c1e33
Jaime Daza <jaimedavid250701@outlook.com>
2023-03-03 20:06:00 -0500

    refactor: Move data from App component to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
 import React, {useState} from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import AppCard from "./components/AppCard";
+import {data} from "./data/card-data";
 import "bootswatch/dist/journal/bootstrap.min.css";
-
-const data = [
-  {
-    img: "https://picsum.photos/1920/1080",
-    title: "Card Title",
-    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    img: "https://picsum.photos/1920/1080",
-    title: "Card Title",
-    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    img: "https://picsum.photos/1920/1080",
-    title: "Card Title",
-    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    img: "https://picsum.photos/1920/1080",
-    title: "Card Title",
-    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    img: "https://picsum.photos/1920/1080",
-    title: "Card Title",
-    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    img: "https://picsum.photos/1920/1080",
-    title: "Card Title",
-    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-];
 
 function App() {
   let rows = Math.ceil(data.length / 4);
---
+++ src/data/card-data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    img: "https://picsum.photos/1920/1080",
+    title: "Card Title",
+    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    img: "https://picsum.photos/1920/1080",
+    title: "Card Title",
+    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    img: "https://picsum.photos/1920/1080",
+    title: "Card Title",
+    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    img: "https://picsum.photos/1920/1080",
+    title: "Card Title",
+    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    img: "https://picsum.photos/1920/1080",
+    title: "Card Title",
+    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    img: "https://picsum.photos/1920/1080",
+    title: "Card Title",
+    body: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+];
